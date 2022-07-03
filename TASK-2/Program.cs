954ab72df79c808322108e7fa9c10819cca3dbaf
Vlad <vlad.melnik@bk.ru>
2022-07-03 13:13:15 +0300

    2 TASK added

---
+++ TASK-2/Program.cs
@@ -0,0 +1,13 @@
+﻿Qube(n);
+void Qube(int num)
+{
+    int current = 1;
+    while(current <= num)
+    {int Qube = current * current * current;
+        Console.WriteLine(Qube);
+        current++;
+    }
+}
+Console.WriteLine("Input a number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
