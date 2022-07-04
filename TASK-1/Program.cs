435dc3e61381e972e16de66999a24532ff234d2f
Vlad <vlad.melnik@bk.ru>
2022-07-04 13:25:35 +0300

    1 Tasl reviewed

--- TASK-1/Program.cs
+++ TASK-1/Program.cs
@@ -1,11 +1,14 @@
 ﻿
 void Palindrom(int num)
-{   if(num/10000 == num%10 && num / 1000 % 10 == (num % 100) / 10)
+{
+    if(num<10000 || num>=100000)
+    Console.WriteLine("There should be only five digits in your number");
+    else
+    if(num/10000 == num%10 && num / 1000 % 10 == (num % 100) / 10)
     Console.WriteLine("YES");
     else 
     Console.WriteLine("NO"); 
-    if(num<1000 && num<100000)
-    Console.WriteLine("There should be only five digits in your number");
+
 }
 Console.WriteLine("Input your number");
 int res = Convert.ToInt32(Console.ReadLine());
